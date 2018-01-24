ae86f957481605375ae7655f12e5e45f26373170
jarrodyellets <jarrod524@gmail.com>
2018-01-24 10:23:20 +0100

    Added flicker

--- js/index.js
+++ js/index.js
@@ -2,19 +2,20 @@ $(document).ready(function(){
   let power = false;
   let saberOn =new Audio("http://www.jarrodyellets.com/sounds/saberOn.mp3");
   let saberOff = new Audio("http://www.jarrodyellets.com/sounds/saberOff.mp3");
-  let hum = new Audio("http://www.jarrodyellets.com/sounds/hum.wav");
   $(".housing").click(function(){
     power = !power;
     if(power){
       $(this).addClass("rotate");
       setTimeout(function(){
         $(".saber").css("width", "1000px");
+        $(".saber").addClass("saberFlicker");
         saberOff.pause();
         saberOff.currentTime = 0;
         saberOn.play();
       }, 300);
     } else {
       $(".saber").css("width", "0");
+      $(".saber").removeClass("saberFlicker");
       saberOn.pause();
       saberOn.currentTime = 0;
       saberOff.play();
